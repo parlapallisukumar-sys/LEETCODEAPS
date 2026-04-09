3263ce499f8d91097e64c6417c1ff805fbf4d8d2
parlapallisukumar-sys <parlapallisukumar@gmail.com>
2026-04-09 09:44:55 +0530

    Time: 127 ms (55.26%), Space: 279.4 MB (71.33%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,27 +1,28 @@
+import java.util.*;
+
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        if (source == destination) return true;
-
-        List<Integer>[] adj = new ArrayList[n];
-        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
-
-        for (int[] e : edges) {
-            adj[e[0]].add(e[1]);
-            adj[e[1]].add(e[0]);
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
         }
-
-        boolean visited[] = new boolean[n];
-        dfs(source,adj, visited);
-
-        return (visited[destination])? true : false; 
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        boolean[] visited = new boolean[n];
+        return dfs(graph, visited, source, destination);
     }
-    private static void dfs(int node, List<Integer>[] adj, boolean[] visited){
-        visited[node] = true;
-        for(int nbr: adj[node]){
-            if(!visited[nbr]){
-                dfs(nbr, adj, visited);
+    private boolean dfs(List<List<Integer>> graph, boolean[] visited, int curr, int dest) {
+        if (curr == dest) return true;
+        visited[curr] = true;
+        for (int neighbor : graph.get(curr)) {
+            if (!visited[neighbor]) {
+                if (dfs(graph, visited, neighbor, dest)) {
+                    return true;
+                }
             }
         }
-
+        return false;
     }
 }
